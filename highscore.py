b95ee9a013d6dfd4e4531d0d456750d86db968f3
VivaanMalik <64945592+VivaanMalik@users.noreply.github.com>
2020-07-05 16:34:44 +0530

    20200705

--- highscore.py
+++
@@ -1,6 +0,0 @@
-import shelve
-d = shelve.open('gameparameters.csv')
-d['gameparameters'] = 'lol'
-d.close()
-
-# comment1
--- setup.py
+++
@@ -1,9 +0,0 @@
-import cx_Freeze
-
-executables = [cx_Freeze.Executable("Falling.py")]
-
-cx_Freeze.setup(
-    name = "Falling",
-    options = {"build_exe": {"packages": ["pygame", "random", "os", "csv", "inspect"], "include_files":["Falling.mp3", "gameparameters.csv"]}},
-    executables = executables
-    )
