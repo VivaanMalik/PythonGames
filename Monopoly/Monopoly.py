765e7ad7e0bb575b65315fbd6511b0a0b2e8a19c
VivaanMalik <64945592+VivaanMalik@users.noreply.github.com>
2020-11-10 11:25:23 +0530

    20201110

---
+++ Monopoly/Monopoly.py
@@ -0,0 +1,30 @@
+#GivingPlayersValue
+#TypesOfFakePeople@Jordindian
+#XD
+Player1name = input(print("Enter name of Player 1"))
+Player2name = input(print("Enter name of Player 2"))
+Player3name = input(print("Enter name of Player 3"))
+Player4name = input(print("Enter name of Player 4"))
+
+Players = [{ "name":Player1name, "current_balance":15000, "position":0}, 
+           { "name":Player2name, "current_balance":15000, "position":0},
+           { "name":Player3name, "current_balance":15000, "position":0},
+           { "name":Player4name, "current_balance":15000, "position":0}]
+Player1 = Players[0]
+Player2 = Players[1]
+Player3 = Players[2]
+Player4 = Players[3]
+print(Player2["name"])
+
+Properties = [{ "name":"Old Kent Road", "Houses":0, "mortgaged":False, "Owner":"Nobody", "Purchase_price":600, "base_rent":20, "mortgage_value":300, "unmortgage_value":330, "house1rent":100, "house2rent":300, "house3rent":900, "house4rent":1600, "Hotelrent":2500, "colorset":40, "house price":500},
+              { "name":"Whitechapel Road", "Houses":0, "mortgaged":False, "Owner":"Nobody", "Purchase_price":600, "base_rent":40, "mortgage_value":300, "unmortgage_value":330, "house1rent":200, "house2rent":600, "house3rent":1800, "house4rent":3200, "Hotelrent":4500, "colorset":80, "house price":500},
+              { "name":"The Angel Islington", "Houses":0, "mortgaged":False, "Owner":"Nobody", "Purchase_price":1000, "base_rent":60, "mortgage_value":500, "unmortgage_value":550, "house1rent":300, "house2rent":900, "house3rent":2700, "house4rent":4000, "Hotelrent":5500, "colorset":120,  "house price":500},
+              { "name":"Euston Road", "Houses":0, "mortgaged":False, "Owner":"Nobody", "Purchase_price":1000, "base_rent":60, "mortgage_value":500, "unmortgage_value":550, "house1rent":300, "house2rent":900, "house3rent":2700, "house4rent":4000, "Hotelrent":5500, "colorset":120,  "house price":500},
+              { "name":"Pentonville Road", "Houses":0, "mortgaged":False, "Owner":"Nobody", "Purchase_price":1200, "base_rent":80, "mortgage_value":600, "unmortgage_value":660, "house1rent":400, "house2rent":1000, "house3rent":3000, "house4rent":4500, "Hotelrent":6000, "colorset":160, "house price":500},
+              { "name":"Pall Mall", "Houses":0, "mortgaged":False, "Owner":"Nobody", "Purchase_price":1400, "base_rent":100, "mortgage_value":700, "unmortgage_value":770, "house1rent":500, "house2rent":1500, "house3rent":4500, "house4rent":6250, "Hotelrent":7500, "colorset":200, "house price":1000},
+              { "name":"White Hall", "Houses":0, "mortgaged":False, "Owner":"Nobody", "Purchase_price":1400, "base_rent":100, "mortgage_value":700, "unmortgage_value":770, "house1rent":500, "house2rent":1500, "house3rent":4500, "house4rent":6250, "Hotelrent":7500, "colorset":200, "house price":1000},
+              { "name":"Northumberland Avenue", "Houses":0, "mortgaged":False, "Owner":"Nobody", "Purchase_price":1600, "base_rent":120, "mortgage_value":800, "unmortgage_value":880, "house1rent":600, "house2rent":1800, "house3rent":5000, "house4rent":7000, "Hotelrent":9000, "colorset":240, "house price":1000}
+                            
+    ]
+
+print(Properties[0]["name"])    
---
+++ Other python progs/ML.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+x1 = int(input("Enter an x position: "))
+y1 = int(input("Enter an y position: "))
+x2 = int(input("Enter an x position: "))
+y2 = int(input("Enter an y position: "))
+x3 = int(input("Enter an x position: "))
+y3 = int(input("Enter an y position: "))
+
+
+print("Blue")
+print("Orange")
+print("Green")
+print("Red")
