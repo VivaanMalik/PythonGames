fbf875dab28eea479f0fcda7307cc10fe7acc3f8
Vivaan Da Gr8 <malikvivaan@gmail.com>
2020-12-16 14:41:31 +0530

    20201216

--- Cell Phone/cell phone.py
+++ Cell Phone/cell phone.py
@@ -8,14 +8,22 @@ pygame.init()
 user32 = ctypes.windll.user32
 screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)-30
 
-gamewindow = pygame.display.set_mode((screensize))
+game = pygame.display.set_mode((screensize))
 pygame.display.set_caption("Life Cycle of a Phone!!!")
 
 def gameloop():
     gamequit=False
+    bg=pygame.image.load('background.png')
+    bg=pygame.transform.scale(bg, (screensize))
     while not gamequit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gamequit = True
+        game.blit(bg, (0, 0))
+
+
+        pygame.display.update()
+        
     pygame.quit()
+    
 gameloop()
